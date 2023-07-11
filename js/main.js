1f3ed7b5d690067582754d5b7bebe8ee4ccb64b6
Edurdo1090 <edujoportiz1090@gmail.com>
2023-07-10 19:10:41 -0600

    ´inicioproyecto´

---
+++ js/main.js
@@ -0,0 +1,25 @@
+const menuprincipal = document.querySelectorAll('.menu-item');
+
+menuprincipal.forEach(function(item){
+    item.addEventListener('mouseover', function(e){
+        const actualItem = document.querySelector('.active');
+        actualItem.classList.remove('active');
+        e.target.classList.add('active');
+        console.log("mouseover");
+
+    });
+});  
+
+
+let caja=document.getElementById("subir__arriba");
+caja.addEventListener("click",function(){
+    document.documentElement.scrollTop=0;
+})
+
+window.addEventListener("scroll",function(){
+    if (document.documentElement.scrollTop > 0) {
+        caja.style.display="flex"
+    } else {
+        caja.style.display="none"
+    }
+})
